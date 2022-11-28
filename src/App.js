06efa135c3307f1027a457f140f6273cefcacf57
Akhil Vinayak P <nirvanaosborn@gmail.com>
2022-11-28 20:41:03 +0530

    test : Routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css"
 import Main from "./components/Main";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App = ()=>{
     return(
         <>
+            <Router>
+                <Routes>
+                    
+                </Routes>
+            </Router>
             <h1 className="bts">JELLL</h1>
             <Main></Main>
         </>
