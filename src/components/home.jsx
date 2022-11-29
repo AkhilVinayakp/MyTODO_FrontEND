4410bb06926ef4b16566b46e6ac1711e18b4b46b
Akhil Vinayak P <nirvanaosborn@gmail.com>
2022-11-29 13:30:52 +0530

    test:

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,17 +1,29 @@
+import { useState } from "react";
 import "./home.css"
 import Login from "./login";
 import Register from "./register";
 
+
 const Home = (props)=>{
-    function invokeToggler(){
+    function invokeToggler(e){
+        console.log(e)
         const toggler = document.querySelector(".container_home");
         console.log("toggling")
-        if(toggler.classList.contains('active')){
+        if(toggler.classList.contains('active') && e.target.className === "close"){
             toggler.classList.remove('active');
         }
         else{
             toggler.classList.add("active");
         }
+    }
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    // for registration
+    const [name, setName] = useState(null);
+    const [regEmail, setRegEmail] = useState(null);
+    const [regPass, setRegPass] = useState(null);
+    const registerUser = ()=>{
+        
     }
     return (
         <div className="mt-24">
@@ -21,13 +33,13 @@ const Home = (props)=>{
             <h1 className="title">Login</h1>
             <form>
                 <div className="input-container_home">
-                <input type="#{type}" id="#{label}" required="required" />
-                <label htmlFor="#{label}">Username</label>
+                <input type="text" id="email_field" required="required" value={email} onChange= {(e)=>setEmail(e.target.value)} />
+                <label htmlFor="email_field">Email</label>
                 <div className="bar" />
                 </div>
                 <div className="input-container_home">
-                <input type="#{type}" id="#{label}" required="required" />
-                <label htmlFor="#{label}">Password</label>
+                <input type="password" id="password_field" required="required" value={password} onChange={(e)=>{setPassword(e.target.value)}} />
+                <label htmlFor="password_field">Password</label>
                 <div className="bar" />
                 </div>
                 <div className="button-container_home">
@@ -41,26 +53,28 @@ const Home = (props)=>{
             <div className="toggle" onClick={invokeToggler} />
             <h1 className="title">
                 Register
-                <div className="close" onClick={invokeToggler} />
+                <div className="close" onClick={invokeToggler}>
+
+                </div>
             </h1>
             <form>
                 <div className="input-container_home">
-                <input type="#{type}" id="#{label}" required="required" />
-                <label htmlFor="#{label}">Username</label>
+                <input type="#{type}" id="reg_name" required="required" value={name} onChange={(e)=>setName(e.target.value)} />
+                <label htmlFor="reg_email">Name</label>
                 <div className="bar" />
                 </div>
                 <div className="input-container_home">
-                <input type="#{type}" id="#{label}" required="required" />
+                <input type="#{type}" id="reg_email" required="required" value={regEmail} onChange={(e)=>{setRegEmail(e.target.value)}} />
+                <label htmlFor="reg_email">Email</label>
+                <div className="bar" />
+                </div>
+                <div className="input-container_home">
+                <input type="#{type}" id="reg_password" required="required" value={regPass} onChange={(e)=>{setRegPass(e.target.value)}} />
                 <label htmlFor="#{label}">Password</label>
                 <div className="bar" />
                 </div>
-                <div className="input-container_home">
-                <input type="#{type}" id="#{label}" required="required" />
-                <label htmlFor="#{label}">Repeat Password</label>
-                <div className="bar" />
-                </div>
                 <div className="button-container_home">
-                <button>
+                <button onClick={registerUser} type="submit">
                     <span>Next</span>
                 </button>
                 </div>
