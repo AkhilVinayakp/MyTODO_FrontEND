697d54aa4a2a68b93108a044a7a0ee080688bcd7
Akhil Vinayak P <nirvanaosborn@gmail.com>
2022-11-30 13:26:42 +0530

    test : loading subtasks

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,35 @@
+import { useState, useEffect, useRef } from "react";
+import autoAnimate from "@formkit/auto-animate";
+import Task from "./task";
 
 const Todo = ({data})=>{
+    const [todo, setTodo] = useState({
+        ...data
+    });
+    const [show, setShow] = useState(false);
+    const parent = useRef(null);
+    useEffect(()=>{
+        parent.current && autoAnimate(parent.current);
+    }, [parent]);
+    const revel = ()=> setShow(!show);
+    const changeSubtaskStatus = ()=>{}
+
     return(
-            <li className="mb-4 w-[620px] h-16 p-1 flex justify-between items-center">
-                <input
-                    type="text"
-                    placeholder="double click to Type"
-                    className=" todo-content input w-full max-w-[540px] input-md"
-                    readOnly=""
-                    value={data.title}
-                />
+            <li className="mb-4 w-[620px]  p-1 flex justify-between items-center">
+                <div className="w-full" ref={parent}>
+                    <input
+                        type="text"
+                        placeholder="double click to Type"
+                        className=" todo-content input w-full max-w-[540px] input-md"
+                        readOnly=""
+                        value={todo.title}
+                        onChange={(e)=>setTodo({...todo, title:e.target.value})}
+                        onClick={revel}
+                    />
+                    {show && <div className="bg-[#2a303c] mt-2 rounded-md w-[85%] p-1 overflow-y-auto float-right mr-8">
+                        {todo.task && todo.task.map((sitem)=>(<Task sdata={sitem} todoKey={data._id} key={sitem._id}></Task>))}
+                    </div>}
+                </div>
                 <div>
                     <button className="btn btn-circle  btn-outline" name="close-btn">
                     <svg
