d242de445a1c8f028676595e93528c933f9e9fce
Akhil Vinayak P <nirvanaosborn@gmail.com>
2022-12-01 11:09:56 +0530

    dev : Delete todo in the database

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -11,7 +11,10 @@ const Todo = ({data})=>{
         ...data
     });
     const [show, setShow] = useState(false);
+    const [todoHide, setTodoHide] = useState(false);
     const parent = useRef(null);
+    const user = JSON.parse(localStorage.getItem("user"));
+    const jwt = localStorage.getItem("jwt");
     useEffect(()=>{
         parent.current && autoAnimate(parent.current);
     }, [parent]);
@@ -23,8 +26,7 @@ const Todo = ({data})=>{
          *   
          */
         const todoTitle = e.target.value;
-        const user = JSON.parse(localStorage.getItem("user"));
-        const jwt = localStorage.getItem("jwt");
+
         if(todoTitle && !todo._id){
             // create new todo in the database
             axios.post(`${API.todoApi}/todoes/${user._id}/create`,{
@@ -51,52 +53,68 @@ const Todo = ({data})=>{
 
     }
     const deleteTodo = (e)=>{
-        
+        setTodoHide(true);
+        // <API.todoapi>/todoes/<userid>/delete/<todoid>
+        axios.delete(`${API.todoApi}/todoes/${user._id}/delete/${todo._id}`,{
+            headers:{
+                Authorization: `Bearer ${jwt}`
+            }
+        }).then((response)=>{
+            console.log("todo delteted successfully")
+            toast(`${todo.title} Removed`);
+
+        }).catch((err)=>{
+            console.log(err);
+            toast("Could not delete from backend please try again after realoading");
+        })
     }
 
     return(
+       <>
+        {!todoHide && (
             <li className="mb-4 w-[620px]  p-1 flex justify-between items-center">
-                <div className="w-full" ref={parent}>
-                    <input
-                        type="text"
-                        placeholder="double click to Type"
-                        className=" todo-content input w-full max-w-[540px] input-md"
-                        readOnly=""
-                        value={todo.title}
-                        onChange={(e)=>setTodo({...todo, title:e.target.value})}
-                        onClick={revel}
-                        onBlur={updateData}
-                    />
-                    {show && <div className="bg-[#2a303c] mt-2 rounded-md w-[85%] p-1 overflow-y-auto float-right mr-8">
-                        {todo.task && todo.task.map((sitem)=>(<Task sdata={sitem} todoKey={data._id} key={sitem._id}></Task>))}
-                    </div>}
-                </div>
-                <div>
-                    {!show ? (                    
-                        <button className="btn btn-circle  btn-outline" name="close-btn" onClick={deleteTodo}>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M6 18L18 6M6 6l12 12"
-                            />
-                        </svg>
-                    </button>):(
-                        <button className="btn btn-circle btn-success" name="close-btn">
-                            <SvgComponent></SvgComponent>
-                        </button>
-                    )}
+            <div className="w-full" ref={parent}>
+                <input
+                    type="text"
+                    placeholder="double click to Type"
+                    className=" todo-content input w-full max-w-[540px] input-md"
+                    readOnly=""
+                    value={todo.title}
+                    onChange={(e)=>setTodo({...todo, title:e.target.value})}
+                    onClick={revel}
+                    onBlur={updateData}
+                />
+                {show && <div className="bg-[#2a303c] mt-2 rounded-md w-[85%] p-1 overflow-y-auto float-right mr-8">
+                    {todo.task && todo.task.map((sitem)=>(<Task sdata={sitem} todoKey={data._id} key={sitem._id}></Task>))}
+                </div>}
+            </div>
+            <div>
+                {!show ? (                    
+                    <button className="btn btn-circle  btn-outline" name="close-btn" onClick={deleteTodo}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M6 18L18 6M6 6l12 12"
+                        />
+                    </svg>
+                </button>):(
+                    <button className="btn btn-circle btn-success" name="close-btn">
+                        <SvgComponent></SvgComponent>
+                    </button>
+                )}
 
-                </div>
-            </li>
-
+            </div>
+        </li>
+        )}
+       </> 
     )
 }
 
