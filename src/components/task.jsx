696785d34e26336483cb290096dc09654bd33670
Akhil Vinayak P <nirvanaosborn@gmail.com>
2022-12-01 13:37:03 +0530

    dev: API handled for deleting subtasks

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -8,8 +8,9 @@ const Task = (props)=>{
      * props.sdata => object corresponding to the current task
      * props.todoKey => cotains the _id of the main task object.  
      */
-    // console.log(props)
+
     const [task, setTask] =  useState(props.sdata);
+    const [taskHide, setTaskHide] = useState(false);
     const mainTodo = props.todoKey;
     const user = JSON.parse(localStorage.getItem("user"));
     const jwt = localStorage.getItem("jwt");
@@ -22,13 +23,8 @@ const Task = (props)=>{
                 _id: "6381070ddf377ed32a04aea3"
             }
      */
-    // ** database interactions
-    function deleteSubTask(){
-        // TODO: delete the current task from loaded list and update to the backend. => useEffect
-
-    }
     const updateSubTaskTitle = (e)=>{
-        // changing / creating subtask title
+        // changing / creating subtask.
         const subTitle = e.target.value;
         if(!task._id && subTitle){
             // create new subtask
@@ -60,18 +56,44 @@ const Task = (props)=>{
             })
 
         }
+        else if(subTitle){
+            // if the current does not match with state update.
+        }
     }
+    const deleteSubTask = ()=>{
+        setTaskHide(true);
+        if(task.subTask){
+            /**
+             * /api/todoes/:uid/todo/:tid/task/:stid  
+             */
+            axios.delete(`${API.todoApi}/todoes/${user._id}/todo/${props.todoKey}/task/${task._id}`,{
+                headers:{
+                    Authorization: `Bearer ${jwt}`
+                }
+            }).then((response)=>{
+                console.log("Subtask deleted.");
+                toast(`${task.subTask} removed`);
+            }).catch((err)=>{
+                console.log(err);
+                toast("Somthing went wrong, Please try again after reloading");
+            })
 
+        }
+    }
     return(
-        <div className="flex justify-center items-center gap-4 w-auto mb-1 py-1 px-1">
-            <input type="text" placeholder="Type here" className="input  input-sm w-full max-w-sm" value={task.subTask} onChange={(e)=>setTask({...task, subTask:e.target.value})} 
-            onBlur={updateSubTaskTitle}
-            />
-            <input type="checkbox" className="checkbox checkbox-success" checked={task.isComplete} onChange={(e)=>setTask({...task, isComplete:e.target.checked})} />
-            <button className="btn btn-circle btn-sm" onClick={deleteSubTask}>
-                <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/></svg>
-            </button>
-        </div>
+        <>
+            {!taskHide && (
+                <div className="flex justify-center items-center gap-4 w-auto mb-1 py-1 px-1">
+                <input type="text" placeholder="Type here" className="input  input-sm w-full max-w-sm" value={task.subTask} onChange={(e)=>setTask({...task, subTask:e.target.value})} 
+                onBlur={updateSubTaskTitle}
+                />
+                <input type="checkbox" className="checkbox checkbox-success" checked={task.isComplete} onChange={(e)=>setTask({...task, isComplete:e.target.checked})} />
+                <button className="btn btn-circle btn-sm" onClick={deleteSubTask}>
+                    <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/></svg>
+                </button>
+            </div>                
+            )}
+        </>
     )
 }
 
